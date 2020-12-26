8db78b8ef90485389164efd484a981036af19ab1
salman <salman.17@iiits.in>
2020-12-26 23:12:31 +0530

    props passed

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,41 @@
-import React from 'react'
+import React, {useState} from 'react'
 import RecipeList from './RecipeList'
 import '../css/app.css'
+import {v4 as uuidv4} from 'uuid'
 
 function App() {
+  function handleRecipeAdd(){
+    const newRecipe = {
+      id:uuidv4(),
+      name:'New',
+      servings:1,
+      cookTime:'1:00',
+      instructions:'Instr.',
+      ingredients:[
+        {id:uuidv4(), name:'Name',amount:'1 tbs' }
+      ]
+    }
+    setRecipes([...recipes, newRecipe])
+  }
+
+  function handleRecipeDelete(id){
+    setRecipes(recipes.filter(recipe => recipe.id !== id))
+  }
+
+  console.log("App component")
+  const [recipes,setRecipes] = useState(sampleRecipes)
   return (
-    <RecipeList recipes={sampleRecipes}>
+    <RecipeList 
+      recipes={recipes}
+      handleRecipeAdd={handleRecipeAdd}
+      handleRecipeDelete={handleRecipeDelete}>
     </RecipeList>
   )
+
+  
+
+  
+
 }
 
 
